d483264d6ba9a1fedca8a85a6349fc7a717f4d5c
jacovanc <jacovancr@gmail.com>
2018-01-12 12:15:26 +0000

    Initial Commit

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+
+    GameObject settingsPanel;
+    GameObject menuPanel;
+    GameObject collapseButton;
+	// Use this for initialization
+	void Start () {
+        settingsPanel = GameObject.Find("SettingsPanel");
+        menuPanel = GameObject.Find("MenuPanel");
+        collapseButton = GameObject.Find("CollapseButton");
+
+        ToggleSettingsPanel();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	} 
+
+    public void ToggleSettingsPanel()
+    {
+        if (settingsPanel.active)
+        {
+            settingsPanel.gameObject.SetActive(false);
+        } else
+        {
+            settingsPanel.gameObject.SetActive(true);
+        }
+    }
+
+    public void ToggleMenuPanel()
+    {
+        Vector3 pos = collapseButton.transform.position;
+        
+        if (menuPanel.active)
+        {
+            menuPanel.gameObject.SetActive(false);
+            pos.x += 100f;
+            collapseButton.transform.position = pos;
+        }
+        else
+        {
+            menuPanel.gameObject.SetActive(true);
+            pos.x -= 100f;
+            collapseButton.transform.position = pos;
+        }
+    }
+}
