64efc1172e14bf93bb25a91702460bd284e8a6e3
jacovanc <jacovancr@gmail.com>
2018-01-12 13:05:23 +0000

    Read gps from text file, convert to GPSPosition list

---
+++ Assets/Scripts/HeightMapScripts/GPSPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSPosition {
+
+    public string Longitude;
+    public string Latitude;
+    public string Altitude;
+
+    public GPSPosition(string longitude, string latitude, string altitude)
+    {
+        Longitude = longitude;
+        Latitude = latitude;
+        Altitude = altitude;
+    }
+}
---
+++ Assets/Scripts/HeightMapScripts/HeightMapGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightMapGenerator : MonoBehaviour {
+
+    public TextAsset GPSData;
+    //private List<GPSPosition> GPSPositions = new List<GPSPosition>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateModel()
+    {
+        var test = ReadFileToArray();
+        foreach(var x in test)
+        {
+            Debug.Log(x.Latitude + " " + x.Longitude + " " + x.Altitude);
+        }
+
+    }
+    List<GPSPosition> ReadFileToArray()
+    {
+        List<string> GPSList = TextAssetToList(GPSData);
+        List<GPSPosition> GPSPositions = new List<GPSPosition>();
+        foreach(string item in GPSList)
+        {
+            var split = item.Split(',');
+            if(split[0] == "$GPGGA")
+            {
+                GPSPosition pos = new GPSPosition(split[2], split[4], split[9]);
+                GPSPositions.Add(pos);
+            }
+        }
+        return GPSPositions;
+    }
+    void CreateHeightMap()
+    {
+
+    }
+
+    private List<string> TextAssetToList(TextAsset ta)
+    {
+        var listToReturn = new List<string>();
+        var arrayString = ta.text.Split('\n');
+        foreach (var line in arrayString)
+        {
+            listToReturn.Add(line);
+        }
+        return listToReturn;
+    }
+
+}
